63e30c42a9294a834314e16abc10cc22f0e0ede7
gauravraj2601 <gauravraj2601@gmail.com>
2023-11-09 20:22:02 +0530

    add

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose")
+require("dotenv").config();
 
-
-const connection = mongoose.connect("mongodb+srv://gaurav:gauravraj@cluster0.390kn1i.mongodb.net/URL_Shortener?retryWrites=true&w=majority")
+const connection = mongoose.connect(process.env.MONGO_URL)
 
 
 module.exports={connection}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express= require("express")
 const cors = require("cors");
+require("dotenv").config();
 const { connection } = require("./db");
 const { urlRouter } = require("./routes/url.route");
 const PORT=8080
@@ -13,7 +14,7 @@ app.get("/",(req,res)=>{
     res.send({"msg":"This is the HOME "})
 })
 
-app.listen(PORT,async()=>{
+app.listen(process.env.PORT,async()=>{
     try {
         await connection
         console.log("Connected to DB")
